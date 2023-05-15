b5686e2940cc129579d69b890c929ecf7635b36f
John Lemak <lemak91@mac.com>
2023-05-14 19:16:39 -0500

    done

--- src/components/Login.js
+++ src/components/Login.js
@@ -22,6 +22,7 @@ const Login = () => {
 
   const login = (e) => {
     e.preventDefault();
+    document.cookie = `loggedIn=true;max-age=60*1000`;
     // set cookie here
     // set loggedIn = true and max-age = 60*1000 (one minute)
 
